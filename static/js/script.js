3af4bb7e21e2efc9e7704f1ea8da0c626afa5d02
MiloszSen <117857272+MiloszSen@users.noreply.github.com>
2025-01-19 22:52:40 +0100

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,4 @@
-// script.js
 
-// Przykładowa funkcja, która może zastąpić 
-// kod z inline <script> w index.html
 function runAlgorithm() {
     const formData = new FormData(document.getElementById("algoForm"));
     
@@ -18,18 +15,15 @@ function runAlgorithm() {
       document.getElementById("profitLabel").innerText = 
         "Najlepszy osiągnięty zysk: " + data.bestProfit.toFixed(2);
 
-      // Wyświetlenie całkowitego czasu wykonania algorytmu
+      
       document.getElementById("timeLabel").innerText = 
         "Całkowity czas wykonania algorytmu: " + data.algTimeAll.toFixed(3) + " s";
 
-      // Opcjonalnie: można tutaj wywołać funkcję do dalszej obróbki danych,
-      // np. rysowanie grafu lub animacji
-      // drawGraphAnimation(data.routeEdges);
+      
     })
     .catch(err => console.error(err));
 }
 
-// Podłączenie obsługi submit formularza
 document.getElementById("algoForm").addEventListener("submit", function(e) {
     e.preventDefault();
     runAlgorithm();
